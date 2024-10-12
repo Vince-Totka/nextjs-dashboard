e61794d66c8b64ed20d3dd50bead837195d646d7
Vince Totka <vincent.totka@gmail.com>
2024-10-11 21:30:12 -0500

    added PPR

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,6 @@ import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
  
@@ -14,15 +13,15 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton/>}>
-          <CardWrapper/>
+          <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton/>}>
-          <RevenueChart/>
+          <RevenueChart />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton/>}>
-          <LatestInvoices/>
+          <LatestInvoices />
         </Suspense>
       </div>
     </main>
